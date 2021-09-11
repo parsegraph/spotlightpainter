15796c58743ff632b6acdbefe1c8af4a8bd57c49
Aaron Faanes <dafrito@gmail.com>
2021-09-10 19:34:50 -0500

    Rename to spotlightpainter

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-DIST_NAME = TODO-PACKAGE-NAME
+DIST_NAME = spotlightpainter
 
 SCRIPT_FILES = \
 	src/index.ts
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var assert = require("assert");
-import todo from "../dist/parsegraph-TODO-PACKAGE-NAME";
+import todo from "../dist/parsegraph-spotlightpainter";
 
 describe("Package", function () {
   it("works", ()=>{
