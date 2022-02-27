76146addf049e30e92992d166a0767ff3bff1497
Aaron Faanes <dafrito@gmail.com>
2022-02-26 21:40:52 -0600

    minor lint

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-var assert = require("assert");
+const assert = require("assert");
 import SpotlightPainter from "../src/index";
 import { BasicGLProvider } from "parsegraph-compileprogram";
 import { mockDOM } from "node-canvas-webgl";
@@ -6,7 +6,7 @@ mockDOM(window);
 
 describe("Package", function () {
   it("works", () => {
-    let ctx = new BasicGLProvider();
+    const ctx = new BasicGLProvider();
     ctx.setExplicitSize(400, 400);
     const painter = new SpotlightPainter(ctx);
     assert.ok(painter);
